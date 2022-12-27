0cc1ed3b697c538c66a7bd2b2565deaef08a593f
atakan-yilmaz <atayilmaz.atakan@gmail.com>
2022-12-27 19:26:25 +0300

    main code
    <>

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -10,11 +10,27 @@ public class TileManager
     //TILE IS OCCUPIED BY SOMETHING?
     public bool occupied;
 
+    public ObstacleType obstacleType;
+
     //THE STUFF THAT THE TILE IS BEING OCCUPIED BY
     public enum ObstacleType
     {
         None,
         Resource,
         Building
+    }
+
+    public void SetOccupied(ObstacleType t, BuildManager bM)
+    {
+        occupied = true;
+        obstacleType = t;
+
+        buildingRef = bM;
+    }
+
+    public void CleanTile()
+    {
+        obstacleType = ObstacleType.None;
+        occupied = false;
     }
 }
