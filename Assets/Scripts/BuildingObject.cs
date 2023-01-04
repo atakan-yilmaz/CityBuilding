8c5a29b4d9d0a9d964fabde886259daf603ecd65
atakan-yilmaz <atayilmaz.atakan@gmail.com>
2023-01-04 16:11:29 +0300

    resource coroutine

--- Assets/Scripts/BuildingObject.cs
+++ Assets/Scripts/BuildingObject.cs
@@ -5,4 +5,40 @@ using UnityEngine;
 public class BuildingObject : MonoBehaviour
 {
     public BuildManager data;
+
+    [Header("Resource Generation")]
+    [Space(8)]
+
+    //THIS WILL BE THE RESOURCE THAT HAS BEED CREATED BY THIS BUILDING 
+    public float resource = 0;
+
+    public int resourceLimit = 100;
+
+    //RESOURCE GENERATE SPEED
+    public float generationSpeed = 5;
+
+    Coroutine buildingBehaviour;
+
+    private void Start()
+    {
+        buildingBehaviour = StartCoroutine(CreateResource());    
+    }
+
+    IEnumerator CreateResource()
+    {
+        //RESOURCES INFINITELY
+        while (true)
+        {
+            if (resource < resourceLimit)
+            {
+                resource += generationSpeed * Time.deltaTime;
+            }
+            else
+            {
+                resource = resourceLimit;
+            }
+
+            yield return null;
+        }
+    }
 }
